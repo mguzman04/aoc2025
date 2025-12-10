e154f4957118dca0851a73b43f6e3b3d9a7584f2
Michael Guzman <mgzmn4@gmail.com>
2025-12-10 08:41:43 -0600

    more initial functionality added for day 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,5 +1,18 @@
 fn invalid_id_sum(input: String) -> i32 {
+    // parse the string into comma seperated strings
+    // let ranges: Vec<&str> = input.rsplit(',').map(|substring| range_to_ints(substring));
     0
+}
+
+fn invalid_ids(range: Vec<&str>) -> Vec<i32> {
+    vec![0]
+}
+
+fn range_to_ints(string_range: &str) -> Vec<i32> {
+    let (min_str, max_str) = string_range.split_once('-').unwrap();
+    let min = min_str.parse::<i32>().unwrap();
+    let max = max_str.parse::<i32>().unwrap();
+    (min..=max).collect()
 }
 
 #[cfg(test)]
@@ -10,5 +23,10 @@ mod test {
     fn test_invalid_sum() {
         let input = "11-22".to_string();
         assert_eq!(invalid_id_sum(input), 2);
+    }
+
+    #[test]
+    fn invalid_ids_test() {
+        unimplemented!()
     }
 }
