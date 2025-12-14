e93465e2d6541570583376ef0b63a42f3842dc8b
Michael Guzman <mgzmn4@gmail.com>
2025-12-14 17:30:57 -0600

    complete more tests

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,13 +1,22 @@
+use regex::Regex;
+
 fn invalid_id_sum(input: String) -> i32 {
     // parse the string into comma seperated strings
     // let ranges: Vec<&str> = input.rsplit(',').map(|substring| range_to_ints(substring));
     0
 }
 
-fn invalid_ids(range: Vec<&str>) -> Vec<i32> {
+/// Takes a vector of integers and returns only the integers that are invalid IDs
+/// Example: Passing [11,12,13,14,15,16,17,18,19,20,21,22] will return [11,22]
+fn invalid_ids(range: Vec<i32>) -> Vec<i32> {
+    // TODO: Use regex (?<!\d)((.)\2+)(?!\d)
+    let regex = Regex::new(r"(?<!\d)((.)\2+)(?!\d)");
     vec![0]
 }
 
+/// Takes a range of integers in the form of strings with the range
+/// seperated with with a '-' and returns a vector of integers in the range (inclusive)
+/// Example: Passing "10-15", will return [10,11,12,13,14,15]
 fn range_to_ints(string_range: &str) -> Vec<i32> {
     let (min_str, max_str) = string_range.split_once('-').unwrap();
     let min = min_str.parse::<i32>().unwrap();
@@ -26,7 +35,23 @@ mod test {
     }
 
     #[test]
-    fn invalid_ids_test() {
-        unimplemented!()
+    fn test_invalid_ids() {
+        let invalid_id_check = vec![11, 22];
+        let range = (11..=22).collect();
+        assert_eq!(
+            invalid_ids(range),
+            invalid_id_check,
+            "Expected invalid IDs to be 11,22."
+        );
+    }
+
+    #[test]
+    fn test_range_to_ints() {
+        let vec_range = vec![10, 11, 12, 13, 14, 15];
+        let returned_range = range_to_ints("10-15");
+        assert_eq!(
+            returned_range, vec_range,
+            "Expected range to be [10,11,12,13,14,15]"
+        );
     }
 }
